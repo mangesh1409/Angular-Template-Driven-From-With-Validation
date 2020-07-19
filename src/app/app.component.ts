9f0e71d131bc5a57049d284402d0b65c2735709d
Mangesh Jadhav <149mpj@gmail.com>
2020-07-19 20:07:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FormWithValidation';
+  Name;  Email;  Number;  status;
+
+  Submitdata(profileForm){
+      // console.log(profileForm);
+      this.Name=profileForm.controls.name.value;
+      this.Email=profileForm.controls.email.value;
+      this.Number=profileForm.controls.mobile.value;
+      this.status=true;
+      profileForm.reset();
+  }
 }
